a02c932a310b7b7ee8e9e0f9fda090ff0ea41053
Valouoff <guillaisvalentin@gmail.com>
2025-05-23 09:20:22 +0200

    Ajout du nombre de pièges aléatoires avec position aléatoire, ajout d'obstacles qui empêchent Thor de passer, et ajout de Thanos qui se déplace aléatoirement sur la grille

--- script.js
+++ script.js
@@ -1,75 +1,112 @@
-let lightX = 6;
-let lightY = 8;
-let initialTX = 10;
-let initialTY = 7;
+"use strict";
+//-------POSITIONS DE THOR ET ECLAIR
+let lightX = 0;
+let lightY = 0;
+let initialTX = 0;
+let initialTY = 0;
+let nbPieges = Math.floor(Math.random()* 3) + 3;
+let nbObstacles = Math.floor(Math.random()* 3) + 3;
 
-let piegeX = Math.floor((Math.random() * 40) + 1);
-let piegeY = Math.floor((Math.random() * 40) + 1);
-let piege2X = Math.floor((Math.random() * 40) + 1);
-let piege2Y = Math.floor((Math.random() * 40) + 1);
 let endGame = false;
 let vies = 3;
 
-// console.log(`Le premier piège se situe en ${piegeX} sur l'axe X et en ${piegeY} sur l'axe Y`);
-// console.log(`Le deuxième piège se situe en ${piege2X} sur l'axe X et en ${piege2Y} sur l'axe Y`);
-console.log(`la position de l'eclair est ${lightX} sur l'axe X et ${lightY} sur l'axe Y`);
+function getRandomX(max) {
+  return Math.floor(Math.random() * max);
+}
+
+function getRandomY(max) {
+  return Math.floor(Math.random() * max);
+}
+
+initialTX = getRandomX(40);
+initialTY = getRandomY(18);
+lightX = getRandomX(40);
+lightY = getRandomY(18);
+
+//-------POSITIONS DE THANOS
+
+let thanosPositionXY = [];
+
+function thanosPosition(nombre) {
+    for (let i = 0 ; i < nombre ; i++) {
+        thanosX = getRandomX(40);
+        thanosY = getRandomY(18);
+        thanosPositionXY.push([thanosX, thanosY])
+    }
+    if ((thanosX > 40) || (thanosX < 0) || (thanosY > 18) || (thanosY < 0)) {
+        console.log("thanos est sortie du plateau");
+    }
+}
+
+let thanosX = lightX -1;
+let thanosY = lightY -1;
+if ((thanosX > 40) || (thanosX < 0) || (thanosY > 18) || (thanosY < 0)) {
+    console.warn("thanos est sortie du plateau");
+    thanosX = lightX +1;
+    thanosY = lightY +1;
+}
+
+//-------POSITIONS DES PIEGES ET OBSTACLES
+
+let pieges = [];
+
+function allPieges(nombre) {
+    for (let i = 0 ; i < nombre ; i++) {
+        let piegeX = getRandomX(40);
+        let piegeY = getRandomY(18);
+        pieges.push([piegeX, piegeY])
+    }
+}
+
+allPieges(nbPieges);
+console.log("Les pièges", pieges);
+
+//----OBSTACLES
+
+let obstacles = [];
+
+function allObstacles(nombre) {
+    for (let i = 0 ; i < nombre ; i++) {
+        let obstaclesX = getRandomX(40);
+        let obstaclesY = getRandomY(18);
+        obstacles.push([obstaclesX, obstaclesY])
+    }
+}
+
+allObstacles(nbObstacles)
+console.log("Les obstacles", obstacles);
+
+//-------JEUX
+
 console.log(`Nombre de vies : ${vies}`);
+console.log(`la position de l'eclair est ${lightX} sur l'axe X et ${lightY} sur l'axe Y`);
 
-
-// let lightX = prompt (`Choisissez la position X de l'éclair`);
-// let lightY = prompt (`Choisissez la position Y de l'éclair`);
-// let initialTX = prompt (`Choisissez la position X de Thor`);
-// let initialTY = prompt (`Choisissez la position Y de Thor`);
-
-// if ((initialTX > 40) || (initialTX < 0) || (initialTY > 18) || (initialTY < 0) || (lightX > 40) || (lightX < 0) || (lightY > 18) || (lightY < 0)) {
-//     alert(`Une ou plusieurs de vos positions se situe en dehors de la zone`);
-//     let endGame = true;
-//     location.reload();
-// }
-
-while (endGame == false) {
+while (!endGame) {
 
     console.log(`la position de thor est ${initialTX} sur l'axe X et ${initialTY} sur l'axe Y`);
-
+    console.log(`la position de Thanos est ${thanosX} sur l'axe X et ${thanosY} sur l'axe Y`);
     let direction = prompt("indiquer une direction : N, NE, E, SE, S, SW, W, NW");
 
     if (direction === "N") {
-
-        initialTY++
-
+        initialTY--
     } else if (direction === "NE") {
-
-        initialTY++
+        initialTY--
         initialTX++
-        
-
     } else if (direction === "E") {
-
         initialTX++
-
     } else if (direction === "SE") {
-
-        initialTY--
+        initialTY++
         initialTX++
-
     } else if (direction === "S") {
-
-        initialTY--
-
+        initialTY++
     } else if (direction === "SW") {
-
-        initialTY--
-        initialTX--
-
-    } else if (direction === "W") {
-
-        initialTX--
-
-    } else if (direction === "NW") {
-
         initialTY++
         initialTX--
-
+    } else if (direction === "W") {
+        initialTX--
+    } else if (direction === "NW") {
+        initialTY--
+        initialTX--
     }
 
     if ((initialTX === lightX) && (initialTY === lightY)) {
@@ -77,21 +114,57 @@ while (endGame == false) {
         alert(`Bien joué vous avez trouver l'éclaire`);
     }
 
-    if ((initialTX === piegeX) && (initialTY === piegeY) || (initialTX === piege2X) || (initialTY === piege2Y)) {
-        vies--
-        console.log(`Vous avez touché un piège... donc vous perdez une vie :(. Ile vous reste ${vies} vies`);
-    }
-    
-    if (vies === 0 ){
-        endGame = true;
-        alert(`Vous avez perdu toutes vos vies, fin de partie !`)
-        location.reload();
+    for (let i = 0 ; i < pieges.length ; i++) {
+        if (initialTX === pieges[i][0] && initialTY === pieges[i][1]) {
+            vies--
+            console.error(`Vous avez touché un piège... donc vous perdez une vie :(. Ils vous reste ${vies} vies`);
+            if (vies === 0 ){
+                endGame = true;
+                alert(`Vous avez perdu toutes vos vies, fin de partie !`)
+                location.reload();
+            }
+        }
     }
 
+    for (let i = 0 ; i < obstacles.length ; i++) {
+        if (initialTX === obstacles[i][0] && initialTY === obstacles[i][1]) {
+            console.warn(`Vous ne pouvez pas passer il y à un obstacle`)
+            if (direction === "N") {
+                initialTY++
+            } else if (direction === "NE") {
+                initialTY++
+                initialTX--
+            } else if (direction === "E") {
+                initialTX--
+            } else if (direction === "SE") {
+                initialTY--
+                initialTX--
+            } else if (direction === "S") {
+                initialTY--
+            } else if (direction === "SW") {
+                initialTY--
+                initialTX++
+            } else if (direction === "W") {
+                initialTX++
+            } else if (direction === "NW") {
+                initialTY++
+                initialTX++
+            }
+        }
+    }
+
+    thanosPosition(1);
+    console.log(thanosPositionXY);
+
     if ((initialTX > 40) || (initialTX < 0) || (initialTY > 18) || (initialTY < 0)) {
-        endGame= true;
+        endGame = true;
         alert(`Perdue, vous êtes sortie de la zone !`);
         location.reload();
     }
 
+    if ((initialTX === thanosX) || (initialTY === thanosY)) {
+        endGame = true;
+        alert(`Perdue, Thanos vous a trouvé !`);
+        location.reload();
+    }
 }
